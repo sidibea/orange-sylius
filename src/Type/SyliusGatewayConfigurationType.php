c4fc9704b193142353391719747cfe1b3573f427
Adama BERTHE <adamaberthe@MacBook-Pro.local>
2022-09-07 21:03:16 +0000

    First push

---
+++ src/Type/SyliusGatewayConfigurationType.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Acme\SyliusExamplePlugin\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+final class SyliusGatewayConfigurationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder->add('api_key', TextType::class);
+    }
+}
